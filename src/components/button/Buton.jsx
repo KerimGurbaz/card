f2f2f64bfe8d4b5f82143f4db0b876a1dda3bbdb
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-10 21:46:59 +0200

    buton

---
+++ src/components/button/Buton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Buton.css";
+
+const Buton = () => {
+  return <div>Buton</div>;
+};
+
+export default Buton;
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Buton from "../button/Buton";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div>
+      Card
+      <Buton />
+    </div>
+  );
+};
+
+export default Card;
